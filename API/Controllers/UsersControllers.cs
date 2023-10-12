0310be208d40d3e2be43fa75e55dac070dde87a2
rembrandtcosta <rembrandt.costa@ccc.ufcg.edu.br>
2023-10-11 21:39:56 -0300

    feat(controllers): add user controller

---
+++ API/Controllers/UsersControllers.cs
@@ -0,0 +1,31 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly DataContext context;
+    public UserController(DataContext context)
+    {
+        this.context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await context.Users.ToListAsync();
+
+        return users;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser?>> GetUser(int id)
+    {
+        return await context.Users.FindAsync(id);
+    }
+}
