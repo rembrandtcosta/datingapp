cadfe9a54a12e4186becae033e9480c000e4f50d
rembrandtcosta <rembrandt.costa@ccc.ufcg.edu.br>
2023-10-16 10:08:32 -0300

    initial commit

--- API/Program.cs
+++ API/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -18,6 +19,10 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x =>
+{
+    x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();//.WithOrigins("http://localhost:4200");
+});
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
